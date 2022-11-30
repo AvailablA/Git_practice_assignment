338d256b3a35dc6cce274beef3170fc48c4e9c7a
AvailablA <manohar.shewale77@gmail.com>
2022-12-01 00:50:40 +0530

    Check_Palindrome

---
+++ code1.js
@@ -0,0 +1,17 @@
+
+function checkPalindrome(N, str) {
+    let bag="";
+    for(let i=0; i < str.length; i++){
+        bag += str[i]
+    }
+    let rev="";
+    for(let j=bag.length-1; j >= 0; j--){
+        rev += bag[i]
+    }
+    
+    if(rev == bag){
+        console.log("Yes");
+    }else{
+        console.log("No")
+    }
+}
